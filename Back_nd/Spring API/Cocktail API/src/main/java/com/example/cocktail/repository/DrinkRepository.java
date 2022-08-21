bc30b680d49ce3580c24bbaa7f0f01075fefad04
njaina <98956159+njaina@users.noreply.github.com>
2022-08-21 13:26:28 +0300

    Delete Back_nd/Spring API/Cocktail API/src/main/java/com/example directory

--- Back_nd/Spring API/Cocktail API/src/main/java/com/example/cocktail/repository/DrinkRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cocktail.repository;
-
-import com.example.cocktail.model.Drinks;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DrinkRepository extends JpaRepository<Drinks,Integer> {
-}
